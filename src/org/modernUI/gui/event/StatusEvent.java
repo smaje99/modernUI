e709e88e6a6011a396715549ddb43e166aeb20e5
Sergio Majé <s.maje@udla.edu.co>
2020-05-28 15:02:45 -0500

    [MODIFIED] StatusEvent from event

--- src/org/modernUI/gui/event/StatusEvent.java
+++ src/org/modernUI/gui/event/StatusEvent.java
@@ -1,8 +1,7 @@
 package org.modernUI.gui.event;
 
-import java.awt.*;
+import java.awt.Component;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class StatusEvent extends MouseEvent {
     public StatusEvent(Component source, int id, long when, int modifiers, int x, int y, int clickCount, boolean popupTrigger, int button) {
